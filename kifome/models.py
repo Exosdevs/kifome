bab3d74710b7f2d31071c76d8ae0a2814d1471df
eonay <eonay.web@gmail.com>
2023-10-06 15:06:01 -0300

    Update models.py

--- kifome/models.py
+++ kifome/models.py
@@ -24,3 +24,41 @@ class Cardapio(models.Model):
     nome = models.CharField(blank=False, null=False, max_length=120)
     descricao = models.TextField(blank=False, null=False)
     data = models.DateField(blank=False, null=False, unique=True)
+
+
+
+class Turma(models.Model):
+    curso = models.CharField(max_length=255)
+    ano = models.CharField(max_length=4)
+    ordem = models.CharField(max_length=1)
+
+    def __str__(self):
+        return self.curso
+
+
+class Aluno(models.Model):
+    cpf = models.CharField(max_length=30)
+    nome = models.CharField(max_length=30)
+    turma = models.ForeignKey(Turma, on_delete=models.CASCADE)
+    restricao = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
+
+
+class Empresa(models.Model):
+    cnpj = models.CharField(max_length=30)
+    nome = models.CharField(max_length=30)
+    senha = models.CharField(max_length=30)
+
+
+class Cardapio(models.Model):
+    nome = models.CharField(max_length=30)
+    descricao = models.CharField(max_length=30)
+    data = models.DateField()
+
+
+class Pedido(models.Model):
+    pessoa = models.ManyToManyField(Aluno)
+    restricao = models.BooleanField(default=False)
+    data = models.DateField()

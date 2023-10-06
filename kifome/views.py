b513f141a401e3d142e6458cb2fa9d3a0994087d
eonay <eonay.web@gmail.com>
2023-10-06 16:59:12 -0300

    ajust model

--- kifome/views.py
+++ kifome/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    context = {
+        'clientes': 'eo'
+    }
+
+    # Render the HTML template main.html with the data in the context variable
+    return render(request, 'main.html', context=context)
+
